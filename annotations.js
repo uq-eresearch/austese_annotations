1ebcf9c3341b33a9fe64f9a9bd0fc6e224365892
Damien Ayers <damien@omad.net>
2013-05-16 15:59:00 +1000

    Update to latest annotator code

--- annotations.js
+++ annotations.js
@@ -2,15 +2,19 @@ function enableAnnotations(){
     // todo check if logged in
     jQuery('[data-id]').waitForImages(function(){
       jQuery('[data-id]').each(function(i, el){
-          if (!el.annotationsEnabled){
-              jQuery(el).annotator()
-              .annotator('addPlugin','Image')
-              .annotator('addPlugin', 'Motivations')
-              .annotator('addPlugin', 'Prov')
-              .annotator('addPlugin','LoreStore');
-              el.annotationsEnabled = true;
-          }
-      });    
-      
-    })
+        enableAnnotationsOnElement(el);
+      });
+    });
+}
+
+function enableAnnotationsOnElement(el) {
+  if (!el.annotationsEnabled){
+      jQuery(el).annotator({'bindToDocument': true})
+      .annotator('addPlugin','Image')
+      .annotator('addPlugin', 'Motivations')
+      .annotator('addPlugin', 'Prov')
+      .annotator('addPlugin', 'CharRangeSelection')
+      .annotator('addPlugin','LoreStore');
+      el.annotationsEnabled = true;
+  }
 }
--- annotationspage.tpl.php
+++ annotationspage.tpl.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="/sites/all/libraries/ext-4.1.1a/resources/css/ext-all.css">
 <link rel="stylesheet" href="/sites/all/libraries/ext-4.1.1a/resources/css/ext-all-gray.css">
+<link rel='stylesheet' href='/<?php print drupal_get_path('module', 'annotations'); ?>//lib/annotator/lib/vendor/jquery.imgareaselect-0.9.10/css/imgareaselect-default.css'>
 <link rel='stylesheet' href='/<?php print drupal_get_path('module', 'annotations'); ?>/lib/annotator.min.css'>
 <?php 
 
@@ -17,11 +18,15 @@ if (property_exists($user,'data')){
 </div>
 <!--  div id="uiplaceholder"></div-->
 
-<div>
+<div data-id="flamboy">
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vel mattis diam. Vestibulum placerat purus vitae elit rutrum ut luctus felis placerat. Quisque sit amet sollicitudin nisl. Aliquam fringilla dolor eget dui venenatis eleifend. In hac habitasse platea dictumst. Praesent bibendum faucibus cursus. Aenean ultricies vehicula augue, eget congue metus ultricies sed. Praesent pretium risus eleifend tellus molestie rhoncus. Pellentesque sodales consectetur faucibus. Quisque nec urna quam, vitae iaculis ante. Phasellus cursus convallis velit id dapibus. Aenean non nunc purus, ut feugiat est. Duis ac leo tellus. Mauris pharetra varius felis.
 </p><p>
 Fusce faucibus dapibus augue, eu varius libero semper at. Donec quis vulputate magna. Cras tristique molestie nunc, luctus porttitor justo egestas accumsan. Nunc et felis lacus. Nullam eu nisi quis nulla varius euismod. Phasellus tempus turpis diam. Mauris pretium rutrum eros, tempus venenatis nisl venenatis ut. Praesent id faucibus nisi. Sed a luctus eros. In sollicitudin cursus molestie. In hac habitasse platea dictumst. Quisque rhoncus purus et magna vestibulum a hendrerit neque auctor. Quisque faucibus dolor id purus condimentum mollis. Phasellus rhoncus, erat eget sagittis pellentesque, ipsum urna sodales nisl, eu sagittis dolor enim ut justo. Donec eros neque, suscipit sit amet sodales eget, tincidunt id ipsum.
-</p><p>
+</p>
+</div>
+<div data-id="washy">
+<h2>Washy</h2>
+<p>
 Proin vitae nisl odio. Sed mattis lobortis libero in egestas. Nam ut aliquam mauris. Donec quis diam tellus, dictum vestibulum mauris. Suspendisse potenti. Maecenas gravida semper lacinia. Phasellus aliquet tellus vitae nisi interdum quis imperdiet massa tempus. Nullam gravida rutrum sollicitudin. Aenean mollis nibh non neque commodo convallis. Aliquam arcu turpis, malesuada nec dapibus sit amet, vehicula at orci. Curabitur id sapien purus.
 </p><p>
 Vestibulum turpis ipsum, rhoncus in placerat sodales, blandit eu tellus. Ut ullamcorper metus vel erat adipiscing pulvinar. Nulla facilisi. Donec sed ante nulla, eu ullamcorper arcu. Sed mollis, risus et posuere pulvinar, sapien lectus ullamcorper nisl, eget ultrices tortor mi ut lectus. Phasellus sodales elit ac dui blandit pharetra at eu enim. Duis a viverra mi. Maecenas odio sem, lobortis sit amet tempus eu, bibendum et orci. Pellentesque ornare, diam vel tincidunt facilisis, turpis elit interdum urna, non fermentum purus lacus eu nisl. Proin vel quam ipsum. In felis metus, fringilla tristique aliquam at, tristique vel justo. Ut sagittis volutpat massa nec feugiat. Maecenas sed odio non mauris porta consectetur ut consectetur est. Etiam aliquam vestibulum sagittis. Donec pellentesque, diam vitae bibendum sagittis, erat nunc dictum quam, nec dapibus sem mauris eu leo.
@@ -29,13 +34,24 @@ Vestibulum turpis ipsum, rhoncus in placerat sodales, blandit eu tellus. Ut ulla
 Sed ut leo dui. Nulla facilisi. Quisque turpis justo, imperdiet at sagittis non, pretium volutpat sem. Pellentesque mattis interdum dui, ac aliquet justo luctus a. Nunc vehicula justo purus. Pellentesque arcu nibh, blandit vitae luctus ac, rhoncus et elit. Suspendisse eget diam nunc, ut laoreet nulla.
 </p>
 </div>
+<div data-id="fish" style="width:65%">
+<img src="/<?php print drupal_get_path('module', 'annotations'); ?>/U1933_UBC-007.jpg">
+</div>
+<div data-id="flamboy">
+<img src="/<?php print drupal_get_path('module', 'annotations'); ?>/1HW_Q2_A3v.png">
+</div>
 <script>
  window.addEventListener("load", function(){
      
-  jQuery(document.body)
+  jQuery("[data-id]")
    .annotator()
    .annotator('addPlugin','Permissions')
-   .annotator('addPlugin', 'Tags');
+   .annotator('addPlugin', 'Tags')
+   .annotator('addPlugin', 'Image')
+   .annotator('addPlugin', 'CharRangeSelection')
+   .annotator('addPlugin', 'LoreStore', {
+     prefix: '/lorestore/endpoint'
+   });
   
  });
 </script>
